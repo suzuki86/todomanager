15b927790a71e039c957e3f2afe856225c120d95
suzuki86 <tsnr0001@gmail.com>
2015-10-04 16:49:41 +0900

    Imprement add() and edit() on TodosController

--- app/Controller/TodosController.php
+++ app/Controller/TodosController.php
@@ -2,11 +2,44 @@
 
 class TodosController extends AppController {
   public function index() {
+    $todos = $this->Todo->find('all',
+      array(
+        'fields' => array('Todo.id', 'Todo.title', 'Todo.detail', 'Todo.created', 'Todo.updated')
+      )
+    );
+    $this->set('todos', $todos);
   }
 
   public function add() {
+    if ($this->request->is('post')) {
+      $this->Todo->create();
+      if ($this->Todo->save($this->request->data)) {
+        $this->Flash->success(__('The todo has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Flash->error(__('The todo could not be saved. Please, try again.'));
+      }
+    }
   }
 
   public function edit() {
+    $this->Todo->id = $id;
+    if (!$this->Todo->exists()) {
+      throw new NotFoundException(__('Invalid todo'));
+    }
+    if ($this->request->is('post') || $this->request->is('put')) {
+      if ($this->Todo->save($this->request->data)) {
+        $this->Flash->success(__('The todo has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Flash->error(__('The todo could not be saved. Please, try again.'));
+      }
+    } else {
+      $this->request->data = $this->Todo->findById($id);
+    }
+  }
+
+  public function delete() {
+    // Implement later.
   }
 }
