f8ae2e74d7d6647c18e77a8c790ffde7302359d1
suzuki86 <tsnr0001@gmail.com>
2015-10-07 07:27:58 +0900

    Implement edit() for ProjectsController

--- app/Controller/ProjectsController.php
+++ app/Controller/ProjectsController.php
@@ -18,7 +18,21 @@ class ProjectsController extends AppController {
     }
   }
 
-  public function edit() {
+  public function edit($id = null) {
+    $this->Project->id = $id;
+    if (!$this->Project->exists()) {
+      throw new NotFoundException(__('Invalid project'));
+    }
+    if ($this->request->is('post') || $this->request->is('put')) {
+      if ($this->Project->save($this->request->data)) {
+        $this->Flash->success(__('The project has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Flash->error(__('The project could not be saved. Please, try again.'));
+      }
+    } else {
+      $this->request->data = $this->Project->findById($id);
+    }
   }
 
   public function delete() {
