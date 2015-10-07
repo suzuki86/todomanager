d39d77f90b3a8a2071c6c58c7d6b9c4db9d58b54
suzuki86 <tsnr0001@gmail.com>
2015-10-08 00:33:07 +0900

    Add seeds.sql for initial data

---
+++ app/Config/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` VALUES (1,'root','$2a$10$RBbBVG1gXS.Wnxt8Mm5/E.Uuw1rL2SMpIs.oM.lvTf1eQj2A.lpmO','2015-10-01 10:00:00','2015-10-01 10:00:00');
+
+INSERT INTO `statuses`(`id`, `label`) VALUES (1, 'open');
+INSERT INTO `statuses`(`id`, `label`) VALUES (2, 'closed');
+
+INSERT INTO `projects`(`id`, `name`, `description`) VALUES (1, 'Default', 'Defalt Project');
